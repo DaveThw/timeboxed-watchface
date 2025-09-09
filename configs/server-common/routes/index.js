003b567d19f8e3232cb4bef1fa0c70454ee7bb11
Dave Thwaites <github@dave.thwaites.org.uk>
2025-09-09 22:03:00 +0100

    Update the deploy app to an Express skeleton app

--- configs/server-common/routes/index.js
+++ configs/server-common/routes/index.js
@@ -1,20 +1,19 @@
-/* global module*/
-let express = require('express');
-let router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    let scriptSrc = req.app.get('env') === 'production' ? '/app.min.js' : '/app.js';
-    res.render('index', { title: 'Timeboxed', scriptSrc: scriptSrc });
+router.get('/', function(req, res, next) {
+  let scriptSrc = req.app.get('env') === 'production' ? '/app.min.js' : '/app.js';
+  res.render('index', { title: 'Timeboxed', scriptSrc: scriptSrc });
 });
 
 router.get('/pebble-apps/timeboxed/config/', function(req, res) {
-    let scriptSrc = req.app.get('env') === 'production' ? '/app.min.js' : '/app.js';
-    res.render('index', { title: 'Timeboxed', scriptSrc: scriptSrc });
+  let scriptSrc = req.app.get('env') === 'production' ? '/app.min.js' : '/app.js';
+  res.render('index', { title: 'Timeboxed', scriptSrc: scriptSrc });
 });
 
 router.get('/healthcheck', function(req, res) {
-    res.status(200).send();
+  res.status(200).send();
 });
 
 module.exports = router;
